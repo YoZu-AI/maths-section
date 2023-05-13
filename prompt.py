15b3accb10402ea2cbb26404436dd571d01f3449
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-13 20:09:56 +0530

    Added more prompt examples

--- prompt.py
+++ prompt.py
@@ -1,4 +1,95 @@
 prompt_template = """
+Q: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
+
+# solution in Python:
+
+
+def solution():
+    #Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
+    money_initial = 23
+    bagels = 5
+    bagel_cost = 3
+    money_spent = bagels * bagel_cost
+    money_left = money_initial - money_spent
+    result = money_left
+    return result
+
+
+
+
+Q: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
+
+# solution in Python:
+
+
+def solution():
+    #Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
+    golf_balls_initial = 58
+    golf_balls_lost_tuesday = 23
+    golf_balls_lost_wednesday = 2
+    golf_balls_left = golf_balls_initial - golf_balls_lost_tuesday - golf_balls_lost_wednesday
+    result = golf_balls_left
+    return result
+
+
+
+
+
+Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
+
+# solution in Python:
+
+
+def solution():
+    #There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
+    computers_initial = 9
+    computers_per_day = 5
+    num_days = 4  # 4 days between monday and thursday
+    computers_added = computers_per_day * num_days
+    computers_total = computers_initial + computers_added
+    result = computers_total
+    return result
+
+
+
+
+
+Q: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
+
+# solution in Python:
+
+
+def solution():
+    Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
+    toys_initial = 5
+    mom_toys = 2
+    dad_toys = 2
+    total_received = mom_toys + dad_toys
+    total_toys = toys_initial + total_received
+    result = total_toys
+    return result
+
+
+
+
+
+Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
+
+# solution in Python:
+
+
+def solution():
+    Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
+    jason_lollipops_initial = 20
+    jason_lollipops_after = 12
+    denny_lollipops = jason_lollipops_initial - jason_lollipops_after
+    result = denny_lollipops
+    return result
+
+
+
+
+
 Q: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
 
 # solution in Python:
@@ -50,4 +141,23 @@ def solution():
 
 Q: {question}
 
-# solution in Python:"""
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
