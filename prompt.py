c7233697f658f3b6f133f857e56f9758f201f0bf
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-14 10:56:58 +0530

    Add files via upload
    Added Symbolic prompt instead of PAL

--- prompt.py
+++ prompt.py
@@ -1,163 +1,29 @@
-prompt_template = """
-Q: Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
-
-# solution in Python:
-
-
-def solution():
-    #Olivia has $23. She bought five bagels for $3 each. How much money does she have left?
-    money_initial = 23
-    bagels = 5
-    bagel_cost = 3
-    money_spent = bagels * bagel_cost
-    money_left = money_initial - money_spent
-    result = money_left
-    return result
-
-
-
-
-Q: Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
-
-# solution in Python:
-
-
-def solution():
-    #Michael had 58 golf balls. On tuesday, he lost 23 golf balls. On wednesday, he lost 2 more. How many golf balls did he have at the end of wednesday?
-    golf_balls_initial = 58
-    golf_balls_lost_tuesday = 23
-    golf_balls_lost_wednesday = 2
-    golf_balls_left = golf_balls_initial - golf_balls_lost_tuesday - golf_balls_lost_wednesday
-    result = golf_balls_left
-    return result
-
-
-
-
-
-Q: There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-
-# solution in Python:
-
-
-def solution():
-    #There were nine computers in the server room. Five more computers were installed each day, from monday to thursday. How many computers are now in the server room?
-    computers_initial = 9
-    computers_per_day = 5
-    num_days = 4  # 4 days between monday and thursday
-    computers_added = computers_per_day * num_days
-    computers_total = computers_initial + computers_added
-    result = computers_total
-    return result
-
-
-
-
-
-Q: Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
-
-# solution in Python:
-
-
-def solution():
-    Shawn has five toys. For Christmas, he got two toys each from his mom and dad. How many toys does he have now?
-    toys_initial = 5
-    mom_toys = 2
-    dad_toys = 2
-    total_received = mom_toys + dad_toys
-    total_toys = toys_initial + total_received
-    result = total_toys
-    return result
-
-
-
-
-
-Q: Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-
-# solution in Python:
-
-
-def solution():
-    Jason had 20 lollipops. He gave Denny some lollipops. Now Jason has 12 lollipops. How many lollipops did Jason give to Denny?
-    jason_lollipops_initial = 20
-    jason_lollipops_after = 12
-    denny_lollipops = jason_lollipops_initial - jason_lollipops_after
-    result = denny_lollipops
-    return result
-
-
-
-
-
-Q: Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
-
-# solution in Python:
-
-
-def solution():
-    #Leah had 32 chocolates and her sister had 42. If they ate 35, how many pieces do they have left in total?
-    leah_chocolates = 32
-    sister_chocolates = 42
-    total_chocolates = leah_chocolates + sister_chocolates
-    chocolates_eaten = 35
-    chocolates_left = total_chocolates - chocolates_eaten
-    result = chocolates_left
-    return result
-
-
-
-
-
-Q: If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
-
-# solution in Python:
-
-
-def solution():
-    #If there are 3 cars in the parking lot and 2 more cars arrive, how many cars are in the parking lot?
-    cars_initial = 3
-    cars_arrived = 2
-    total_cars = cars_initial + cars_arrived
-    result = total_cars
-    return result
-
-
-
-
-
-Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
-
-# solution in Python:
-
-
-def solution():
-    #There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?
-    trees_initial = 15
-    trees_after = 21
-    trees_added = trees_after - trees_initial
-    result = trees_added
-    return result
-
+prompt_template = '''
+Let's solve mathematical word problems in a careful, formal manner. The solution will follow the Peano format: 
+1- Each sentence in the solution either introduces a new variable or states a new equation. 
+2- The last sentence gives the goal: which variable will contain the answer to the problem. 
+3- Each equation only uses previously introduced variables. 
+4- Each quantity is only named by one variable.
+5- Use all the numbers in the question.
+Q: Mary had 5 apples. The next morning, she ate 2 apples. Then, in the afternoon, she bought as many apples as she had after eating those apples in the morning. How many apples did she end up with?
+Peano solution:
+Let a be the number of apples Mary started with [[var a]]. We have [[eq a = 5]]. 
+Let b be how many apples she had in the morning after eating 2 apples [[var b]]. We have [[eq b = a - 2]]. 
+Let c be the apples she bought in the afternoon [[var c]]. 
+Since she bought as many as she had after eating, we have [[eq c = b]]. 
+Let d be how many apples she ended up with [[var d]]. We have [[eq d = b + c]]. 
+The answer is the value of d [[answer d]]. 
+Q: Mario and Luigi together had 10 years of experience in soccer. Luigi had 3 more than Mario. How many did Mario have?
+Peano solution:
+Let a be the number of years Mario had [[var a]]. 
+Let b be the number of years Luigi had [[var b]]. We have [[eq a + b = 10]]. We also have [[eq b = a + 3]]. 
+The answer is the value of a [[answer a]].
+Q: The planet Goob completes one revolution after every 2 weeks. How many hours will it take for it to complete half a revolution?
+Peano solution:
+Let a be the number of hours in a week [[var a]]. We have [[eq a = 168]]. 
+Let b be the number of hours in a revolution [[var b]]. We have [[eq b = a * 2]]. 
+Let c be the number of hours in half a revolution [[var c]]. We have [[eq c = b / 2]]. 
+The answer is the value of c [[answer c]].
 Q: {question}
-
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Peano solution:
+'''.strip() + '\n\n\n'
