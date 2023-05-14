eeff62d810da4180158755351e0b3a79c4ecf797
sathyakrishna-sharma <68376764+sathyakrishna-sharma@users.noreply.github.com>
2023-05-14 10:56:12 +0530

    Add files via upload
    Implemented Symbolic paper instead of PAL

--- main.py
+++ main.py
@@ -4,45 +4,24 @@ from config import math_model, API_KEY
 openai.api_key = ''
 
 def maths_solution(input_question, student_class):
-  try:
-    prompt = prompt_template.format(question = input_question)
-    completion = openai.ChatCompletion.create(model = math_model,temperature = 0,
-        messages=[{'role': 'system', 'content': 'You are a helpful assistant that can write Python code that solves mathematical reasoning questions similarly to the examples that you will be provided.'},
-        {'role': 'user', 'content': prompt}])
-    python_function = completion['choices'][0]['message']['content']
-    completion2 = openai.ChatCompletion.create(
-          model = 'gpt-3.5-turbo',
-          temperature = 0,
-          messages=[{'role': 'system', 'content': 'You are a python compiler who strictly provides only the answer'},
-        {'role': 'user', 'content': python_function}])
-    final_answer = completion2['choices'][0]['message']['content']
-    lines = python_function.split('return')[0].split('def solution():')[1].split('\n')
-    step_num = 1
-    for i, line in enumerate(lines):
-        num_hashes = line.count('#')
-        if num_hashes > 0:
-            lines[i] = line.replace('#'*num_hashes, f"Step {step_num}: ")
-            step_num += 1
-    output_str = '\n'.join(lines) + '\n' + str(final_answer)
-    return output_str
-  except:
-    maths_prompt = 'explain the solution to the following maths problem, {question}. Provide solution in a step by step approach such that a student of class {student_class_no} can understand'
-    input_prompt = maths_prompt.format(question = input_question, student_class_no = student_class)
-    #print(input_prompt)
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        temperature=0,
-        messages=[
-        {"role": "system", "content": "You are a kind and helpful AI maths tutor you provide solution in step by step manner"},
-        {"role": "user", "content": input_prompt}
-        ]
-        )
-    return completion['choices'][0]['message']['content']
-  ######################################################
-  
-#################################################
+  prompt = prompt_template.format(question = input_question)
+  completion = openai.ChatCompletion.create(
+    model = math_model,
+    temperature = 0,
+    messages=[
+      {"role": "user", "content": prompt }])
+  symbol = completion.choices[0].message.content
+  math_prompts = 'Analyze the given input, {symbolic_steps}. Provide the solution for the given input. You should also explain the soultion in precise, concise and step by step approach so that a student of class {student_class_no} can understand the solution'
+  input_prompt = math_prompts.format(symbolic_steps = symbol, student_class_no = student_class)
+  second_completion = openai.ChatCompletion.create(
+    model = math_model,
+    temperature = 0,
+    messages=[
+      {"role": "system", "content": 'you are an expert maths symbolic interpreter and AI tutor who provide solution to given input and explains the solution' },
+      {"role": "user", "content": input_prompt}])
+  return(second_completion.choices[0].message.content)
+#############################
 
-input_question = 'A bag contains 5 red balls and some blue balls. If the probability of drawing a blue ball is double that of a red ball, determine the number of blue balls in the bag.'
-solution = maths_solution(input_question,'8')
-print(solution)
-     
+input_question = '(2*2+7)*8'
+class_no = '10'
+print(maths_solution(input_question, class_no))
